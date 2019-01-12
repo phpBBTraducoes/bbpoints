c446664d4f43558e25cdd52e8e1d238a26b2b9c3
eunaumtenhoid <eunaumtenhoid@outlook.com>
2019-01-12 01:25:52 -0200

    ADD

---
+++ language/pt_br/bbpoints_common.php
@@ -0,0 +1,36 @@
+<?php
+/**
+*
+* BBpoints extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2015 SiteSplat All rights reserved
+* @license Proprietary
+* Brazilian Portuguese translation by eunaumtenhoid (c) 2019 [ver 1.2.1] (https://github.com/phpBBTraducoes)
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if ( empty($lang) || !is_array($lang) )
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'BBPOINTS'						=> 'BBpoints', // This is the forum point name. You can edit it to reflect your own point name.
+	'YOUR_BBPOINTS'					=> 'Seus BBpoints',
+	'BB_AMOUNT'			    		=> 'Quantidade',
+	'BB_PAID_UP'			    	=> 'Pago',
+	'TOPIC_HIDDEN'					=> 'Tópico oculto. Requer um pagamento',
+	'WAS_HIDDEN'					=> 'Este é um tópico pago',
+	'BBPOINTS_HIDE_TOPIC'	        => 'Pontos mín.',
+	'BBPOINTS_HIDE_TOPIC_EXPLAIN'	=> 'Digite os Pontos Mínimos necessários para o próximo usuário ler este tópico. <br />(Os pontos serão transferidos para sua conta)',
+	'TRANSFER_NOTI'					=> 'Você recebeu uma doação de %1$s de %2$s',
+
+	'NOTIFICATION_TYPE_SITESPLAT.BBPOINTS.NOTIFICATION.TYPE.TRANSFER' => 'Alguém transferiu Pontos para você',
+));
